4e8e7068b434a6d58a9c4cb92008099d56703a39
Bryan Robinson <bryanlrobinson@gmail.com>
2016-01-05 13:21:11 -0600

    Added logo and played with background for 'header'

--- index.php
+++ index.php
@@ -7,11 +7,43 @@
 </head>
 <body>
 
-<?php $message = 'Hello World!'; ?>
+<header>
+    <div class="logo">
+        <img src="img/webworkers.svg" alt="Web Workers" />
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">Hello World</a></li>
+            <li><a href="#">Hello World</a></li>
+            <li><a href="#">Hello World</a></li>
+            <li><a href="#">Hello World</a></li>
+        </ul>
+    </nav>
+    <div class="social">
+        <a href="http://twitter.com/memphisww">Twitter</a>
+        <a href="https://www.facebook.com/memphiswebworkers/">Facebook</a>
+    </div>
+</header>
 
-<h1><?= $message ?></h1>
+<section class="main-content">
+    <article class="upcoming">
+        <h2>Upcoming</h2>
+    </article>
 
+    <article class="events">
+        <h2>Other events</h2>
+    </article>
 
+    <article class="podcast">
+        <h2>Podcast</h2>
+    </article>
+
+    <article class="contact">
+        <h2>Get in Touch</h2>
+    </article>
+</section>
+
+</div>
 
 <script src="/js/build/bundle.js"></script>
 
