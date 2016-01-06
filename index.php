cf9290ae740f01bf805f2eed9ca09c1df573b66e
Bryan Robinson <bryanlrobinson@gmail.com>
2016-01-06 13:30:08 -0600

    Added some starter styles to events section and changed out body font and header fonts

--- index.php
+++ index.php
@@ -30,19 +30,63 @@
     <article class="upcoming">
         <div class="background-container">
 
-            <h2>Upcoming</h2>
-            <h3>Title</h3>
+            <h2>Upcoming Meetup</h2>
+            <h3>New Years Resolutions</h3>
             <p>Meet with people in Memphis interested in web design, front-end development, and user experience (UX) design. </p>
             <p>Join us in January for a roundtable discussion on New Year's resolutions!  What are your goals for the new year? Learning a new framework? Sketching every day? Venturing into back-end programming (gasp!)?</p>
             <p>If you're excited about new tech, trends, & tools coming out next year, be sure to join us and share with the group!</p>
             <p>We'll be meeting at CoWork in Midtown (902 S. Cooper). Food and drink provided by Vaco Memphis.</p>
-            <a href="#" title="Find out more" class="button">Find out more &raquo;</a>
+            <a href="#" title="Find out more" class="button">RSVP Here &raquo;</a>
         </div>
 
     </article>
 
     <article class="events">
-        <h2>Other events</h2>
+        <div class="background-container">
+            <h2>Nodebots day</h2>
+            <!-- <div class="logos">
+                <img src="/img/socialwebworkers.svg" alt="Web Workers" /> <img src="/img/hm-logo.svg" />
+            </div> -->
+            <p>HACKmemphis and Memphis Web Workers will be putitng on a Nodebots day in 2016! We're really excited about the intersection of JavaScript and hardware and are ready to make some robots bend to our will!</p>
+            <!-- Begin MailChimp Signup Form -->
+            <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+            <style type="text/css">
+              /*  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; } */
+                /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+                   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+            </style>
+            <div id="mc_embed_signup" class="row align-center">
+                <div class="small-10 columns">
+                <form action="//HACKmemphis.us9.list-manage.com/subscribe/post?u=c2f50ac1ae03bc1caf4c342ca&amp;id=e2b54ec9db" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll">
+                        <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                        <div class="mc-field-group">
+                            <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                            </label>
+                            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                        </div>
+                        <div class="mc-field-group">
+                            <label for="mce-FNAME">First Name </label>
+                            <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                        </div>
+                        <div class="mc-field-group">
+                            <label for="mce-LNAME">Last Name </label>
+                            <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+                        </div>
+                        <div id="mce-responses" class="clear">
+                            <div class="response" id="mce-error-response" style="display:none"></div>
+                            <div class="response" id="mce-success-response" style="display:none"></div>
+                        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c2f50ac1ae03bc1caf4c342ca_e2b54ec9db" tabindex="-1" value=""></div>
+                        <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                    </div>
+                </form>
+            </div>
+            </div>
+            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+            <!--End mc_embed_signup-->
+    </div>
+
     </article>
 
     <article class="podcast">
